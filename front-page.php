c66ad4182934dce9724173cc24a82ad73b222482
Lisa DeBona <annalisa.quiamco@gmail.com>
2022-01-08 03:37:50 -0500

    Styling homepage

--- front-page.php
+++ front-page.php
@@ -10,11 +10,14 @@ get_header();
     <main id="main" class="site-main">
 
       <?php while ( have_posts() ) : the_post(); ?>
-        <header class="entry-header">
+        <header class="entry-header" style="display:none">
           <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
         </header>
 
         <div class="entry-content">
+          <div class="intro text-center">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
           <?php the_content(); ?>
         </div><!-- .entry-content -->
 
